0ec54afb0092b4ce273f6388a49a0b73e0858674
aseme <aseme@wings-ict-solutions.eu>
2025-03-23 10:12:52 +0200

    Init Angular Joke Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-import { ChartsComponent } from "./charts/charts.component";
-import {  TableComponent } from './table/table.component';
-
+import { SidebarComponent } from './main-components/sidebar/sidebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './main-components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +14,20 @@ import {  TableComponent } from './table/table.component';
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    ChartsComponent,
-    TableComponent
+    SidebarComponent,
+    FooterComponent,
+    MatToolbarModule,
+
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'energy';
+  @ViewChild('sidebar') sidebar!: SidebarComponent;
+
+
+  toggleSidebar() {
+    this.sidebar.toggleSidenav();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideStore } from '@ngrx/store';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideEffects } from '@ngrx/effects';
-import { cryptoReducer } from './store/crypto.reducer';
-import { CryptoEffects } from './store/crypto.effects';
-import { ApiService } from './services/api.service'; // ✅ Ensure this is imported
+import { ApiService } from './services/api.service';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideStore({ crypto: cryptoReducer }), // ✅ Ensure correct state key
-    provideEffects([CryptoEffects]), // ✅ Provide NgRx Effects
-    provideHttpClient(withFetch()), // ✅ Enable fetch API for better SSR compatibility
-    ApiService // ✅ Ensure ApiService is available globally
+    ApiService,
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
   ]
 };
