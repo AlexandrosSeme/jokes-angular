01b8b7c3eb493c07b8937d328b7186d0e7c7a967
aseme <aseme@wings-ict-solutions.eu>
2025-03-23 10:23:45 +0200

    remove old page tutorial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { TutorialsComponent } from './pages/tutorials/tutorials.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'tutorials', component: TutorialsComponent }
+    { path: 'home', component: HomeComponent }
 ];
